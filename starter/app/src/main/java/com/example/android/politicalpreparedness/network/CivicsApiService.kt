cf7a57b5b45351458c21c54bb15ef5ff564dc781
osamasalam <osamasalam256@gmail.com>
2023-06-02 20:46:43 +0300

    remove last commit and merge

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -1,25 +1,15 @@
 package com.example.android.politicalpreparedness.network
 
-import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
-import com.example.android.politicalpreparedness.network.models.ElectionResponse
-import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
-import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
-import java.util.*
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
 
-// adapters for Java Date and custom adapter ElectionAdapter (included in project)
+// TODO: Add adapters for Java Date and custom adapter ElectionAdapter (included in project)
 private val moshi = Moshi.Builder()
-        .add(Date::class.java, Rfc3339DateJsonAdapter().nullSafe())
-        .add(ElectionAdapter())
         .add(KotlinJsonAdapterFactory())
         .build()
 
@@ -35,20 +25,11 @@ private val retrofit = Retrofit.Builder()
  */
 
 interface CivicsApiService {
-    // elections API Call
-    @GET("elections")
-    suspend fun getElections(): ElectionResponse
-    //voterinfo API Call
-    @GET("voterinfo")
-    suspend fun getVoterInfo(
-        @Query("electionId") electionId: Int,
-        @Query("address") address: String
-    ): VoterInfoResponse
-    //representatives API Call
-    @GET("representatives")
-    suspend fun getRepresentatives(
-        @Query("address") address: String
-    ) : RepresentativeResponse
+    //TODO: Add elections API Call
+
+    //TODO: Add voterinfo API Call
+
+    //TODO: Add representatives API Call
 }
 
 object CivicsApi {
